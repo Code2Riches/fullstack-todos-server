d305b07c4c25895b201f7d025ddd1d4c34999bef
Code2Riches <chris.benites@gmail.com>
2022-11-10 10:26:22 -0500

    Components, Layouts, Pages

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ require("dotenv").config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var toDoRouter = require('./routes/mocktodos');
 
 // Add the following Mongo import/code to app.js (before the var app = express() code) in order to call the mongo connection
 var { mongoConnect } = require('./mongo.js');
@@ -32,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/mocktodos', toDoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
